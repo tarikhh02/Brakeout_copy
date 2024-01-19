95e55b8d23f991a7b21f78b5bd1e39b5f1b0daa9
tarikhh02 <tarik.hajhamdo@gmail.com>
2024-01-19 15:56:39 +0100

    Fullscreen fix
    Window opens now with full screen

--- Win.cpp
+++ Win.cpp
@@ -52,7 +52,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	RegisterClass(&windowClass);
 
 	//Create window
-	HWND window = CreateWindow(windowClass.lpszClassName, L"My first game", WS_OVERLAPPEDWINDOW | WS_MAXIMIZE | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, startWindowWidth, startWidnowHeight, 0, 0, hInstance, 0);
+	HWND window = CreateWindow(windowClass.lpszClassName, L"My first game", WS_OVERLAPPEDWINDOW | WS_MAXIMIZE | WS_MAXIMIZEBOX | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, startWindowWidth, startWidnowHeight, 0, 0, hInstance, 0);
 	HDC hdc = GetDC(window);
 
 	while (game.isGameRunning)
