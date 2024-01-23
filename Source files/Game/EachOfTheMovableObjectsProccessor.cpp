58a6e97c2f5fb61b66d183afad485c640f2721e0
tarikhh02 <tarik.hajhamdo@gmail.com>
2024-01-23 02:59:30 +0100

    Reverted delta time

--- Source files/Game/EachOfTheMovableObjectsProccessor.cpp
+++ Source files/Game/EachOfTheMovableObjectsProccessor.cpp
@@ -5,7 +5,7 @@ void Game::ProcessEachMovableObject(ObjectBase* object, PhysicsVelocity* physics
 	int lastXPos = object->xPos;
 	int lastYPos = object->yPos;
 
-	if(deltaTimeCounterValue > 0.0021)
+	if(deltaTimeCounterValue > 0.0025)
 	{
 		Physics::MoveObject(object, physicsVelocityComponent, deltaTimeCounterValue, renderer.bufferWidth, renderer.bufferHeight);
 
